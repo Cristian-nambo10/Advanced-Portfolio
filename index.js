4f1a0daa961ddcb69ad9c8a48901b3a41972a08f
Cristian Nambo <nambocristian10@gmail.com>
2022-08-06 21:16:00 -0500

    wip: adds js email functionality

---
+++ index.js
@@ -0,0 +1,24 @@
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_7wmlrlp",
+      "template_iefds7q",
+      event.target,
+      "FRKrYF78j0Pm981qG"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me diretly on ..."
+      );
+    });
+}
